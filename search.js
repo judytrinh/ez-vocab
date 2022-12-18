46763b5c8dd93d8c6bf725bd5a52aa3c573d34ab
Judy Trinh <jtrinh74@gmail.com>
2022-12-18 14:44:27 -0500

    Update to new naver dict and add monolingual dict

--- search.js
+++ search.js
@@ -3,7 +3,9 @@ input.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
         window.open(`https://www.google.com/search?tbm=isch&q=${input.value}`);
-        window.open(`https://en.dict.naver.com/#/search?query=${input.value}`);
+        //window.open(`https://en.dict.naver.com/#/search?query=${input.value}`);
+	window.open(`https://korean.dict.naver.com/koendict/#/search?range=korean&query=${input.value}`);
+	//window.open(`https://ko.dict.naver.com/#/search?query=${input.value}`);
         window.open(`https://korean.dict.naver.com/koendict/#/search?query=${input.value}`);
         window.open(`https://forvo.com/word/${input.value}/#ko`);
     }
